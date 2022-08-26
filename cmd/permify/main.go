0af769b19dd70e83444967578668f10bcca4b781
Tolga Özen <mtolgaozen@gmail.com>
2022-08-26 13:40:00 +0300

    Update main.go

--- cmd/permify/main.go
+++ cmd/permify/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	`fmt`
 	"log"
 
 	"github.com/Permify/permify/internal/app"
 	"github.com/Permify/permify/internal/config"
+)
+
+const (
+	version = "v0.0.0-alpha2"
+	color   = "\033[0;37m%s\033[0m"
+	banner  = `
+██████╗ ███████╗██████╗ ███╗   ███╗██╗███████╗██╗   ██╗
+██╔══██╗██╔════╝██╔══██╗████╗ ████║██║██╔════╝╚██╗ ██╔╝
+██████╔╝█████╗  ██████╔╝██╔████╔██║██║█████╗   ╚████╔╝ 
+██╔═══╝ ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██╔══╝    ╚██╔╝  
+██║     ███████╗██║  ██║██║ ╚═╝ ██║██║██║        ██║   
+╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝        ╚═╝   
+High performance authorization service %s
+`
 )
 
 func main() {
@@ -13,6 +28,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Config error: %s", err)
 	}
+
+	log.Printf(color, fmt.Sprintf(banner, version))
 
 	// Run
 	app.Run(cfg)
