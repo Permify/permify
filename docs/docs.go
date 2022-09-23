f36bedb335b9b970a22302f64fdbc04cc368ea77
Tolga Özen <mtolgaozen@gmail.com>
2022-09-23 11:39:13 +0300

    swagger updates

--- docs/docs.go
+++ docs/docs.go
@@ -710,7 +710,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "v0.0.0-alpha3",
+	Version:          "v0.0.0-alpha4",
 	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{"http", "https"},
