8350dc0d326acf075a9544094ab1171ef9167daf
Tolga Özen <mtolgaozen@gmail.com>
2022-09-29 13:14:25 +0300

    Update error.go

--- internal/errors/error.go
+++ internal/errors/error.go
@@ -11,8 +11,10 @@ var (
 
 	// validation errors
 
-	DepthError              = errors.ValidationError.AddParam("depth", "depth not enough")
-	UndefinedChildTypeError = errors.ValidationError.AddParam("schema", "undefined child type")
-	UndefinedChildKindError = errors.ValidationError.AddParam("schema", "undefined child kind")
-	ActionCannotFoundError  = errors.ValidationError.AddParam("action", "action can not found")
+	DepthError                 = errors.ValidationError.AddParam("depth", "depth not enough")
+	UndefinedChildTypeError    = errors.ValidationError.AddParam("schema", "undefined child type")
+	UndefinedChildKindError    = errors.ValidationError.AddParam("schema", "undefined child kind")
+	ActionCannotFoundError     = errors.ValidationError.AddParam("action", "action can not found in schema")
+	SchemaCannotFoundError     = errors.ValidationError.AddParam("schema", "schema can not found")
+	EntityTypeCannotFoundError = errors.ValidationError.AddParam("entity", "entity type can not found in schema")
 )
