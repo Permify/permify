6a65a1a8630185dc3aaebfc27135aa703b59be4f
Tolga Özen <mtolgaozen@gmail.com>
2022-11-18 17:54:27 +0300

    Update common.go

--- internal/repositories/postgres/utils/common.go
+++ internal/repositories/postgres/utils/common.go
@@ -12,7 +12,10 @@ func SnapshotQuery(sl squirrel.SelectBuilder, revision uint64) squirrel.SelectBu
 		squirrel.Expr(fmt.Sprintf("pg_visible_in_snapshot(created_tx_id, (select snapshot from transactions where id = '%v'::xid8)) = true", revision)),
 		squirrel.Expr(fmt.Sprintf("created_tx_id = '%v'::xid8", revision)),
 	}).Where(squirrel.And{
-		squirrel.Expr(fmt.Sprintf("pg_visible_in_snapshot(expired_tx_id, (select snapshot from transactions where id = '%v'::xid8)) OR (expired_tx_id = '0'::xid8) = false", revision)),
+		squirrel.Or{
+			squirrel.Expr(fmt.Sprintf("pg_visible_in_snapshot(expired_tx_id, (select snapshot from transactions where id = '%v'::xid8)) = false", revision)),
+			squirrel.Expr("expired_tx_id = '0'::xid8"),
+		},
 		squirrel.Expr(fmt.Sprintf("expired_tx_id <> '%v'::xid8", revision)),
 	})
 }
