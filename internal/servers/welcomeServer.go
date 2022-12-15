71507ef52af150906c8d93adcc6a9d217e7b3d9e
Tolga Özen <mtolgaozen@gmail.com>
2022-12-15 21:03:05 +0300

    Update welcomeServer.go

--- internal/servers/welcomeServer.go
+++ internal/servers/welcomeServer.go
@@ -3,8 +3,9 @@ package servers
 import (
 	"context"
 
-	v1 "github.com/Permify/permify/pkg/pb/base/v1"
 	"google.golang.org/protobuf/types/known/emptypb"
+
+	v1 "github.com/Permify/permify/pkg/pb/base/v1"
 )
 
 // WelcomeServer - Structure for Welcome Server
@@ -18,7 +19,6 @@ func NewWelcomeServer() *WelcomeServer {
 }
 
 func (r *WelcomeServer) Hello(context.Context, *emptypb.Empty) (*v1.WelcomeResponse, error) {
-
 	return &v1.WelcomeResponse{
 		Permify: "Open-source authorization service inspired by Google Zanzibar",
 		Sources: &v1.WelcomeResponse_Sources{
@@ -32,5 +32,4 @@ func (r *WelcomeServer) Hello(context.Context, *emptypb.Empty) (*v1.WelcomeRespo
 			Linkedin: "https://github.com/Permify/permify",
 		},
 	}, nil
-
 }
