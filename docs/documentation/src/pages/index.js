c0bb86a0e8ba00fe0a652ee70a5f38dc4780baba
EgeAytin <ege.aytin@gmail.com>
2023-01-26 16:36:16 -0800

    Update head tags in LP

--- docs/documentation/src/pages/index.js
+++ docs/documentation/src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from "@theme/Layout";
 import Head from "@docusaurus/Head";
+import Layout from "@theme/Layout";
 
 import { Landing } from "../components/landing";
 
@@ -20,6 +20,13 @@ function Home() {
                 <html data-page="index" data-customized="true" />
                 <title>Permify | Open Source Authorization Service Based on Google Zanzibar</title>
                 <meta name="Description" content="Open Source Authorization Service Based on Google Zanzibar" />
+                 {/* Load google fonts */}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=DM+Sans:wght@400&family=Inter:wght@400;500;600&family=Manrope:wght@700&family=Noto+Serif+KR:wght@700;900&display=swap"
+                />
             </Head>
             <Landing />
         </>
