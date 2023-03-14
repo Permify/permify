d6f8a57c4ccb1819779197a70db353ca96855de4
Tolga Ozen <mtolgaozen@gmail.com>
2023-03-13 22:38:29 -0700

    docs: swagger version update

--- internal/info.go
+++ internal/info.go
@@ -2,7 +2,7 @@ package internal
 
 const (
 	// Version is the last release of the Permify (e.g. v0.1.0)
-	Version = "v0.3.3"
+	Version = "v0.3.4"
 
 	// Banner is the view for terminal.
 	Banner = `
