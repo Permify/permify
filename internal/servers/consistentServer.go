f3e17c5d3d7adcc6d28453a193418ec3ae9b697e
Murat Mirgun Ercan <muratmirgunercan225@gmail.com>
2023-04-26 16:03:23 +0300

    refactor: consistent server keys engine update

--- internal/servers/consistentServer.go
+++ internal/servers/consistentServer.go
@@ -2,13 +2,9 @@ package servers
 
 import (
 	"errors"
-	"fmt"
-	"github.com/Permify/permify/pkg/cache"
-	"github.com/Permify/permify/pkg/tuple"
-	"github.com/cespare/xxhash/v2"
+	"github.com/Permify/permify/internal/keys"
 	"google.golang.org/grpc/status"
 
-	base "github.com/Permify/permify/pkg/pb/base/v1"
 	otelCodes "go.opentelemetry.io/otel/codes"
 	"golang.org/x/net/context"
 
@@ -20,12 +16,12 @@ import (
 type ConsistentServer struct {
 	v1.UnimplementedConsistentServer
 
-	cacheService cache.Cache
+	cacheService keys.EngineKeyManager
 	logger       logger.Interface
 }
 
 // NewConsistentServer - Creates new Consistent Server
-func NewConsistentServer(s cache.Cache, l logger.Interface) *ConsistentServer {
+func NewConsistentServer(s keys.EngineKeyManager, l logger.Interface) *ConsistentServer {
 	return &ConsistentServer{
 		cacheService: s,
 		logger:       l,
@@ -38,42 +34,36 @@ func (r *ConsistentServer) Get(ctx context.Context, request *v1.ConsistentGetReq
 	defer span.End()
 
 	var err error
-	response, ok := r.cacheService.Get(request.GetKey())
+	response, ok := r.cacheService.GetCheckKey(request.PermissionCheckRequest)
 	if !ok {
-		err = errors.New("key not found" + request.GetKey())
+		err = errors.New("key not found" + request.PermissionCheckRequest.TenantId)
 		span.RecordError(err)
 		span.SetStatus(otelCodes.Error, err.Error())
-		r.logger.Error(err.Error())
+		r.logger.Error("ConsistentServer.Get: " + err.Error())
 		return nil, status.Error(GetStatus(err), err.Error())
 	}
 
 	return &v1.ConsistentGetResponse{
-		PermissionCheckResponse: response.(*base.PermissionCheckResponse),
+		PermissionCheckResponse: response,
+		PermissionCheckRequest:  request.GetPermissionCheckRequest(),
 	}, nil
 }
 
 // Set - Set created Key
-func (r *ConsistentServer) Set(ctx context.Context, request *v1.ConsistentPostRequest) (*v1.ConsistentPostResponse, error) {
+func (r *ConsistentServer) Set(ctx context.Context, request *v1.ConsistentSetRequest) (*v1.ConsistentSetResponse, error) {
 	ctx, span := tracer.Start(ctx, "consistent.set")
 	defer span.End()
 
-	checkKey := fmt.Sprintf("check_%s_%s:%s:%s@%s", request.PermissionCheckRequest.GetTenantId(), request.PermissionCheckRequest.GetMetadata().GetSchemaVersion(), request.PermissionCheckRequest.GetMetadata().GetSnapToken(), tuple.EntityAndRelationToString(&base.EntityAndRelation{
-		Entity:   request.PermissionCheckRequest.GetEntity(),
-		Relation: request.PermissionCheckRequest.GetPermission(),
-	}), tuple.SubjectToString(request.PermissionCheckRequest.GetSubject()))
-
-	h := xxhash.New()
-
-	// Write the checkKey string to the hash object
-	size, err := h.Write([]byte(checkKey))
-	if err != nil {
-		// If there's an error, return false
-		return nil, nil
+	ok := r.cacheService.SetCheckKey(request.PermissionCheckRequest, request.PermissionCheckResponse)
+	if !ok {
+		err := errors.New("key not set" + request.PermissionCheckRequest.TenantId)
+		span.RecordError(err)
+		span.SetStatus(otelCodes.Error, err.Error())
+		r.logger.Error("ConsistentServer.Set: " + err.Error())
+		return nil, status.Error(GetStatus(err), err.Error())
 	}
 
-	r.cacheService.Set(request.GetKey(), request.PermissionCheckRequest, int64(size))
-
-	return &v1.ConsistentPostResponse{
+	return &v1.ConsistentSetResponse{
 		Value: "OK",
 	}, nil
 }
