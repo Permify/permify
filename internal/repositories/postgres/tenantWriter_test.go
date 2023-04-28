c5ea1033ec11744048714b85265cebd2aecabfd3
Murat Mirgun Ercan <muratmirgunercan225@gmail.com>
2023-04-28 20:59:41 +0300

    test: add build tag for unit test

--- internal/repositories/postgres/tenantWriter_test.go
+++ internal/repositories/postgres/tenantWriter_test.go
@@ -1,10 +1,12 @@
+//go:build !integration
+
 package postgres_test
 
 import (
 	"context"
 	"github.com/Masterminds/squirrel"
-	postgres2 "github.com/Permify/permify/internal/repositories/postgres"
-	"github.com/Permify/permify/pkg/database/postgres"
+	"github.com/Permify/permify/internal/repositories/postgres"
+	PQRepository "github.com/Permify/permify/pkg/database/postgres"
 	"testing"
 	"time"
 
@@ -14,27 +16,25 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateTenantWriter_Test(t *testing.T) {
-	// Mock veritabanı bağlantısı oluştur
+func TestCreateTenant_Test(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	require.NoError(t, err)
 	defer db.Close()
 
-	pg := &postgres.Postgres{
+	pg := &PQRepository.Postgres{
 		DB:      db,
 		Builder: squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar),
 	}
 
-	// Logger oluştur
+	// Create Logger
 	log := logger.New("debug")
 
-	// TenantWriter oluştur
-	writer := postgres2.NewTenantWriter(pg, log)
+	// Create TenantWriter
+	writer := postgres.NewTenantWriter(pg, log)
 
-	// Test için context
 	ctx := context.Background()
 
-	// CreateTenant testi
+	// Create Fields for CreateTenant
 	id := "2"
 	name := "tenant_1"
 	createdAt := time.Now()
@@ -48,4 +48,43 @@ func TestCreateTenantWriter_Test(t *testing.T) {
 	assert.NotNil(t, tenant)
 	assert.Equal(t, id, tenant.Id)
 	assert.Equal(t, name, tenant.Name)
+}
+
+func TestDeleteTenant_Test(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer db.Close()
+
+	pg := &PQRepository.Postgres{
+		DB:      db,
+		Builder: squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar),
+	}
+
+	// Create Logger
+	log := logger.New("debug")
+
+	// Create TenantWriter
+	writer := postgres.NewTenantWriter(pg, log)
+
+	ctx := context.Background()
+
+	// Create Fields for CreateTenant
+	id := "2"
+	name := "tenant_1"
+	createdAt := time.Now()
+
+	// SQL sorgusunu bekleyen mock.ExpectQuery oluştur
+	mock.ExpectQuery("INSERT INTO tenants \\(id, name\\) VALUES \\(\\$1,\\$2\\) RETURNING created_at").WithArgs(id, name).
+		WillReturnRows(sqlmock.NewRows([]string{"created_at"}).AddRow(createdAt))
+
+	tenant, err := writer.CreateTenant(ctx, id, name)
+
+	mock.ExpectQuery("DELETE FROM tenants WHERE id = \\$1 RETURNING name, created_at").WithArgs(tenant.Id).
+		WillReturnRows(sqlmock.NewRows([]string{"name", "created_at"}).AddRow(tenant.Name, tenant.CreatedAt.AsTime()))
+
+	deletedTenant, err := writer.DeleteTenant(ctx, tenant.Id)
+	require.NoError(t, err)
+	assert.NotNil(t, deletedTenant)
+	assert.Equal(t, id, deletedTenant.Id)
+
 }
