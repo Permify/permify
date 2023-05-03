db9efa07f9b914062c3bb2e9de1bcdf6321f17df
Murat Mirgun Ercan <muratmirgunercan225@gmail.com>
2023-05-03 16:26:18 +0300

    add: add -c flag add for start with config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -188,6 +188,41 @@ func NewConfig() (*Config, error) {
 	return cfg, nil
 }
 
+// NewConfigWithFile initializes and returns a new Config object by reading and unmarshalling
+// the configuration file from the given path. It falls back to the DefaultConfig if the
+// file is not found. If there's an error during the process, it returns the error.
+func NewConfigWithFile(dir string) (*Config, error) {
+	// Start with the default configuration values
+	cfg := DefaultConfig()
+
+	// Set the name and type of the config file to be read
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	// Add the path where the config file is located
+	viper.AddConfigPath(dir)
+	// Read the config file
+	err := viper.ReadInConfig()
+	// If there's an error during reading the config file
+	if err != nil {
+		// Check if the error is because of the config file not being found
+		if ok := errors.As(err, &viper.ConfigFileNotFoundError{}); !ok {
+			// If it's not a "file not found" error, return the error with a message
+			return nil, fmt.Errorf("failed to load server config: %w", err)
+		}
+		// If it's a "file not found" error, the code will continue and use the default config
+	}
+
+	// Unmarshal the configuration data into the Config struct
+	if err = viper.Unmarshal(cfg); err != nil {
+		// If there's an error during unmarshalling, return the error with a message
+		return nil, fmt.Errorf("failed to unmarshal server config: %w", err)
+	}
+
+	// Return the populated Config object
+	return cfg, nil
+}
+
 // DefaultConfig - Creates default config.
 func DefaultConfig() *Config {
 	return &Config{
