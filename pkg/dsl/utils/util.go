c451e000d11304daf3bb0ec6e3fbdb75b49cbf4f
Tolga Ozen <mtolgaozen@gmail.com>
2023-06-19 23:54:56 +0300

    refactor: unused func removed

--- pkg/dsl/utils/util.go
+++ pkg/dsl/utils/util.go
@@ -2,29 +2,7 @@ package utils
 
 import (
 	"strings"
-
-	"github.com/Permify/permify/pkg/dsl/ast"
-	"github.com/Permify/permify/pkg/dsl/token"
 )
-
-// GetBaseEntityRelationTypeStatement - get entity reference statement from relation type statements
-func GetBaseEntityRelationTypeStatement(sts []ast.RelationTypeStatement) ast.RelationTypeStatement {
-	for _, st := range sts {
-		if ast.IsDirectEntityReference(st) {
-			return st
-		}
-	}
-	return ast.RelationTypeStatement{
-		Sign: token.Token{
-			Type:    token.SIGN,
-			Literal: "@",
-		},
-		Type: token.Token{
-			Type:    token.IDENT,
-			Literal: "user",
-		},
-	}
-}
 
 // Key -
 func Key(v1, v2 string) string {
