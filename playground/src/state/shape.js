470614317e217af91e4b05625ff01b2bf3a707d3
Tolga Ozen <mtolgaozen@gmail.com>
2023-08-18 10:50:49 +0300

    feat: zustand for state-management

---
+++ playground/src/state/shape.js
@@ -0,0 +1,282 @@
+import {create} from 'zustand';
+import axios from 'axios'; // Assuming you're using axios as the "client"
+import yaml from 'js-yaml';
+import {nanoid} from "nanoid";
+import Upload from "../services/s3";
+
+export const useShapeStore = create((set, get) => ({
+    // main shape items
+    schema: ``,
+    relationships: [],
+    attributes: [],
+    scenarios: [],
+
+    graph: {nodes: [], edges: []},
+    loading: false,
+
+    // definitions
+    definitions: {},
+
+    // errors
+    systemError: "",
+    schemaError: null,
+    relationshipErrors: [],
+    attributeErrors: [],
+    visualizerError: "",
+    scenariosError: "",
+
+    setSchema: (schema) => {
+        set({schema: schema})
+
+        setTimeout(() => {
+            get().run()
+        }, 500);
+    },
+
+    addRelationships: (relationships) => {
+        set((state) => ({relationships: [...state.relationships, ...relationships]}));
+
+        setTimeout(() => {
+            get().run()
+        }, 500);
+    },
+
+    removeRelationship: (relationship) => {
+        set((state) => ({
+            relationships: state.relationships.filter(rid => rid !== relationship)
+        }));
+
+        setTimeout(() => {
+            get().run()
+        }, 500);
+    },
+
+    addAttributes: (attributes) => {
+        set((state) => ({attributes: [...state.attributes, ...attributes]}));
+
+        setTimeout(() => {
+            get().run()
+        }, 500);
+    },
+
+    removeAttribute: (attribute) => {
+        set((state) => ({
+            attributes: state.attributes.filter(aid => aid !== attribute)
+        }));
+
+        setTimeout(() => {
+            get().run()
+        }, 500);
+    },
+
+    run: () => {
+
+        get().clearErrors()
+
+        const shape = {
+            schema: get().schema,
+            relationships: get().relationships,
+            attributes: get().attributes,
+            scenarios: get().scenarios,
+        }
+
+        Run(JSON.stringify(shape, null, 2))
+            .then((rr) => {
+                for (let i = 0; i < rr.length; i++) {
+                    const error = JSON.parse(rr[i]);
+
+                    if (error.type === 'schema') {
+                        set({schemaError: handleSchemaError(error.message)});
+                    }
+
+                    if (error.type === 'relationships') {
+                        set((state) => ({relationshipErrors: [...state.relationshipErrors, error]}));
+                    }
+
+                    if (error.type === 'attributes') {
+                        set((state) => ({attributeErrors: [...state.attributeErrors, error]}));
+                    }
+                }
+
+                // If there were no schema errors, proceed to visualize
+                if (!rr.some(error => JSON.parse(error).type === 'schema')) {
+                    return Visualize();
+                } else {
+                    // You can add additional error handling or set state as needed
+                    set({
+                        graph: {nodes: [], edges: []},
+                    });
+                }
+            }).then((vr) => {
+            if (vr) {  // Only execute if vr exists (means no errors previously)
+                set({
+                    graph: JSON.parse(vr[0]),
+                    definitions: JSON.parse(vr[1]),
+                });
+            }
+        }).catch((error) => {
+            set({systemError: error})
+        });
+    },
+
+    // Clear the state
+    clear: () => set({
+        schema: ``,
+        relationships: [],
+        attributes: [],
+        graph: {nodes: [], edges: []},
+        systemError: "",
+        schemaError: null,
+        visualizerError: "",
+        relationshipErrors: [],
+        attributeErrors: [],
+        scenariosError: ""
+    }),
+
+    // Clear the state
+    clearErrors: () => set({
+        systemError: "",
+        schemaError: null,
+        visualizerError: "",
+        relationshipErrors: [],
+        attributeErrors: [],
+        scenariosError: ""
+    }),
+
+    fetchShape: async (pond) => {
+        try {
+            get().clear()
+
+            const response = await axios.get(`https://s3.amazonaws.com/permify.playground.storage/shapes/${pond}.yaml`);
+            const result = yaml.load(response.data, null);
+
+            get().setSchema(result.schema ?? ``);
+            get().addRelationships(result.relationships ?? []);
+            get().addAttributes(result.attributes ?? []);
+
+        } catch (error) {
+            set({systemError: error})
+        }
+    },
+
+    importShape: async (pond) => {
+
+    },
+
+    exportShape: async (pond) => {
+
+    },
+
+    getEntityTypes: () => {
+        const entityDefinitions = get()?.definitions?.entityDefinitions;
+        return Object.keys(entityDefinitions || {});
+    },
+
+    getRelations: (entityType) => {
+        const relations = get()?.definitions?.entityDefinitions?.[entityType]?.relations;
+        return Object.keys(relations || {});
+    },
+
+    getSubjectTypes: (entityType, relation) => {
+        const references = get()?.definitions?.entityDefinitions?.[entityType]?.relations?.[relation]?.relationReferences;
+
+        if (!references || references.length === 0) {
+            return [];
+        }
+
+        const uniqueTypes = new Set(references.map(ref => ref?.type).filter(Boolean));
+        return [...uniqueTypes];
+    },
+
+    getSubjectRelations: (entityType, relation, subjectType) => {
+        const references = get()?.definitions?.entityDefinitions?.[entityType]?.relations?.[relation]?.relationReferences;
+
+        if (!references || references.length === 0) {
+            return [];
+        }
+
+        return references
+            .filter(v => v?.type === subjectType)
+            .map(v => v?.relation)
+            .filter(Boolean);
+    },
+
+    getAttributes: (entityType) => {
+        const attributes = get()?.definitions?.entityDefinitions?.[entityType]?.attributes;
+        return Object.keys(attributes || {});
+    },
+
+    getTypeValueBasedOnAttribute: (entityType, attribute) => {
+        const type = get()?.definitions?.entityDefinitions?.[entityType]?.attributes[attribute]?.type;
+        switch (type) {
+            case "ATTRIBUTE_TYPE_BOOLEAN":
+                return "boolean"
+            case "ATTRIBUTE_TYPE_STRING":
+                return "string"
+            case "ATTRIBUTE_TYPE_INTEGER":
+                return "integer"
+            case "ATTRIBUTE_TYPE_DOUBLE":
+                return "double"
+            default:
+                return ""
+        }
+    },
+
+    shareShape: async () => {
+        let id = nanoid()
+        const state = get();
+
+        const yamlString = yaml.dump({
+            schema: state.schema,
+            relationships: state.relationships,
+            scenarios: state.scenarios
+        });
+
+        const file = new File([yamlString], `shapes/${id}.yaml`, {type: 'text/x-yaml'});
+        return Upload(file)
+    }
+}));
+
+// Handles schema errors.
+const handleSchemaError = (res) => {
+    // Get line and column numbers from the error message.
+    let numbers = parseNumbers(res);
+
+    return {
+        schemaError: {
+            line: numbers[0],       // Error's line number.
+            column: numbers[1],     // Error's column number.
+            // Convert the message to a more readable format.
+            message: res.replaceAll('_', ' ').toLowerCase()
+        },
+    };
+};
+
+// Gets two numbers from the start of a string.
+const parseNumbers = (input) => {
+    // Looks for two numbers separated by a colon.
+    const regex = /^(\d+):(\d+)/;
+    const match = regex.exec(input);
+
+    if (match) {
+        // If found, return the numbers.
+        return [parseInt(match[1], 10), parseInt(match[2], 10)]
+    } else {
+        // If not found, return [0, 0].
+        return [0, 0]
+    }
+}
+
+function Run(shape) {
+    return new Promise((resolve) => {
+        let res = window.run(shape)
+        resolve(res);
+    });
+}
+
+function Visualize() {
+    return new Promise((resolve) => {
+        let res = window.visualize()
+        resolve(res);
+    });
+}
--- playground/src/views/Play.js
+++ playground/src/views/Play.js
@@ -1,72 +1,51 @@
 import React, {Suspense, useEffect, useState} from 'react'
-import {useLocation, useNavigate} from "react-router-dom";
-import yaml from "js-yaml";
-import axios from "axios";
-import {shallowEqual, useDispatch, useSelector} from "react-redux";
+import {useLocation} from "react-router-dom";
 import {Skeleton} from "antd";
 import {ContextLayout} from "../utility/context/Layout";
-import PlayOutput from "../layout/sides/outputs/playOutput";
-import FrameOutput from "../layout/sides/outputs/frameOutput";
-import {setRelationships, setSchema} from "../redux/shape/actions";
+import Output from "../layout/sides/output";
+import {useShapeStore} from "../state/shape";
 
-const client = axios.create();
+function Play() {
+    const { fetchShape } = useShapeStore();
 
-function Play(props) {
     let location = useLocation();
-    let navigate = useNavigate();
-
-    const dispatch = useDispatch();
-    const shape = useSelector((state) => state.shape, shallowEqual);
 
     const [type, setType] = useState("");
 
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        setLoading(true)
+        setLoading(true);
+
         const params = new URLSearchParams(location.search);
-        if (params.has("t")){
-            setType(params.get('t'))
+
+        if (params.has("t")) {
+            setType(params.get('t'));
         }
+
         if (params.has('s')) {
-            let search = params.get('s');
-            client.get(`https://s3.amazonaws.com/permify.playground.storage/shapes/${search}.yaml`).then((response) => {
-                return yaml.load(response.data, null)
-            }).then((result) => {
-                dispatch(setSchema(result.schema ?? ``));
-                dispatch(setRelationships(result.relationships ?? []));
-                setLoading(false);
-            }).catch((error) => {
-                navigate('/404')
-            });
+            const search = params.get('s');
+            fetchShape(search).finally(() => setLoading(false));
         } else {
-            window.location = window.location.href.split('?')[0] + `?s=organizations-hierarchies`
+            const baseUrl = window.location.href.split('?')[0];
+            window.location = `${baseUrl}?s=organizations-hierarchies`;
         }
     }, []);
 
     return (
         <ContextLayout.Consumer>
-            {context => {
-                if (type === 'f') {
-                    let LayoutTag = context.fullLayout;
+            {
+                context => {
+                    const LayoutTag = (type === 'f') ? context.fullLayout : context.mainLayout;
                     return (
                         <LayoutTag>
-                            <Suspense fallback={<Skeleton active/>}>
-                                <FrameOutput loading={loading} shape={shape}/>
+                            <Suspense fallback={<Skeleton active />}>
+                                <Output loading={loading} />
                             </Suspense>
                         </LayoutTag>
-                    )
-                } else {
-                    let LayoutTag = context.mainLayout;
-                    return (
-                        <LayoutTag>
-                            <Suspense fallback={<Skeleton active/>}>
-                                <PlayOutput loading={loading} shape={shape}/>
-                            </Suspense>
-                        </LayoutTag>
-                    )
+                    );
                 }
-            }}
+            }
         </ContextLayout.Consumer>
     )
 }
