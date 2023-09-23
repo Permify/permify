d39fc4fb9ec9d87cd263aa8c3cb73c3537027c80
Tolga Ozen <mtolgaozen@gmail.com>
2023-09-23 10:16:04 -0700

    refactor(playground): adjust nodeSpacing from 150 to 220

--- playground/src/pkg/Editor/perm/syntax-definition.js
+++ playground/src/pkg/Editor/perm/syntax-definition.js
@@ -2,7 +2,7 @@ function SyntaxDefinition() {
     return {
         keywords: ["entity", "action", "relation", "and", "or", "not", "permission", "rule", "attribute"],
         escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
-        symbols:  /[=><!~?:&|+\-*\/\^%]+/,
+        symbols:  /[=><!~?:&|+\-*^%]+/,
         tokenizer: {
             root: [
                 [/\B@\w+#?\w+/, 'reference'],
