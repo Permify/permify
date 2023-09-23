17686d775bb35c66ddfefb25fd064d44f050d88c
Tolga Ozen <mtolgaozen@gmail.com>
2023-09-23 10:14:01 -0700

    refactor(playground): add environment variables for S3 configuration

--- playground/craco.config.js
+++ playground/craco.config.js
@@ -1,9 +1,21 @@
 const CracoLessPlugin = require("craco-less");
 
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+}, {});
+
 module.exports = {
+    webpack: {
+        plugins: [
+            new webpack.DefinePlugin(envKeys)
+        ]
+    },
     plugins: [
-        {
-            plugin: CracoLessPlugin,
-        },
-    ],
+        { plugin: CracoLessPlugin }
+    ]
 };
--- playground/src/services/s3.js
+++ playground/src/services/s3.js
@@ -1,22 +1,19 @@
 import AWS from 'aws-sdk'
 
-const S3_BUCKET = 'permify.playground.storage';
-const REGION = 'us-east-1';
-
 AWS.config.update({
-    accessKeyId: '{{ACCESS_KEY}}',
-    secretAccessKey: '{{SECRET_KEY}}'
-})
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY
+});
 
 const storage = new AWS.S3({
-    params: {Bucket: S3_BUCKET},
-    region: REGION,
+    params: {Bucket: process.env.S3_BUCKET},
+    region: process.env.REGION,
 })
 
 export default function Upload(file) {
     const params = {
         Body: file,
-        Bucket: S3_BUCKET,
+        Bucket: process.env.S3_BUCKET,
         Key: file.name,
         ContentType: file.type,
     };
