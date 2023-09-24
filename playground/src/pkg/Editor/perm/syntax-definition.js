879a0602859693f2562abd88e2d2d0bd49296a6e
Tolga Ozen <mtolgaozen@gmail.com>
2023-09-24 11:37:01 -0700

    feat: in keyword added to perm syntax definition

--- playground/src/pkg/Editor/perm/syntax-definition.js
+++ playground/src/pkg/Editor/perm/syntax-definition.js
@@ -1,13 +1,12 @@
 function SyntaxDefinition() {
     return {
-        keywords: ["entity", "action", "relation", "and", "or", "not", "permission", "rule", "attribute"],
+        keywords: ["entity", "action", "relation", "and", "or", "not", "permission", "rule", "attribute", "in"],
         escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
         symbols:  /[=><!~?:&|+\-*^%]+/,
         tokenizer: {
             root: [
                 [/\B@\w+#?\w+/, 'reference'],
                 [/\b(integer|double|boolean|string)\b/, 'type'],
-                [/&&|==|!=|<=|>=|<|>|!|-|%|in|not in/, 'operator'],
                 [/`(?:[^`\\]|\\.)*`/, 'option'],
                 [/@?[a-zA-Z][\w$]*/, {
                     cases: {
@@ -15,6 +14,7 @@ function SyntaxDefinition() {
                         "@default": 'variable'
                     }
                 }],
+                [/&&|==|!=|<=|>=|<|>|!|-|%|in|not in/, 'operator'],
                 { include: '@whitespace' },
                 [/".*?"/, 'string'],
             ],
