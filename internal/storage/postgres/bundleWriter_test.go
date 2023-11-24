74010472bbe85c7840aeeb65766c854a8e300b72
Tolga Ozen <mtolgaozen@gmail.com>
2023-11-24 18:33:36 +0300

    test: bundle writer

---
+++ internal/storage/postgres/bundleWriter_test.go
@@ -0,0 +1,103 @@
+package postgres
+
+import (
+	`context`
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	`os`
+
+	`github.com/Permify/permify/pkg/database`
+	PQDatabase `github.com/Permify/permify/pkg/database/postgres`
+	base `github.com/Permify/permify/pkg/pb/base/v1`
+)
+
+var _ = Describe("BundleWriter", func() {
+	var db database.Database
+	var bundleWriter *BundleWriter
+	var bundleReader *BundleReader
+
+	BeforeEach(func() {
+		version := os.Getenv("POSTGRES_VERSION")
+
+		if version == "" {
+			version = "14"
+		}
+
+		db = postgresDB(version)
+		bundleWriter = NewBundleWriter(db.(*PQDatabase.Postgres))
+		bundleReader = NewBundleReader(db.(*PQDatabase.Postgres))
+	})
+
+	AfterEach(func() {
+		err := db.Close()
+		Expect(err).ShouldNot(HaveOccurred())
+	})
+
+	Context("Write", func() {
+		It("should write and read DataBundles with correct relationships and attributes", func() {
+			ctx := context.Background()
+
+			bundles := []*base.DataBundle{
+				{
+					Name: "user_created",
+					Arguments: []string{
+						"organizationID",
+						"companyID",
+						"userID",
+					},
+					Operations: []*base.Operation{
+						{
+							RelationshipsWrite: []string{
+								"organization:{{.organizationID}}#member@company:{{.companyID}}#admin",
+								"organization:{{.organizationID}}#member@user:{{.userID}}",
+								"organization:{{.organizationID}}#admin@user:{{.userID}}",
+							},
+							RelationshipsDelete: []string{
+								"company:{{.companyID}}#admin@user:{{.userID}}",
+							},
+							AttributesWrite: []string{
+								"organization:{{.organizationID}}$public|boolean:true",
+							},
+							AttributesDelete: []string{
+								"organization:{{.organizationID}}$balance|double:120.900",
+							},
+						},
+					},
+				},
+			}
+
+			names, err := bundleWriter.Write(ctx, "t1", bundles)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(names).Should(Equal([]string{"user_created"}))
+
+			bundle, err := bundleReader.Read(ctx, "t1", "user_created")
+			Expect(err).ShouldNot(HaveOccurred())
+
+			Expect(bundle.GetName()).Should(Equal("user_created"))
+			Expect(bundle.GetArguments()).Should(Equal([]string{
+				"organizationID",
+				"companyID",
+				"userID",
+			}))
+
+			Expect(bundle.GetOperations()[0].RelationshipsWrite).Should(Equal([]string{
+				"organization:{{.organizationID}}#member@company:{{.companyID}}#admin",
+				"organization:{{.organizationID}}#member@user:{{.userID}}",
+				"organization:{{.organizationID}}#admin@user:{{.userID}}",
+			}))
+
+			Expect(bundle.GetOperations()[0].RelationshipsDelete).Should(Equal([]string{
+				"company:{{.companyID}}#admin@user:{{.userID}}",
+			}))
+
+			Expect(bundle.GetOperations()[0].AttributesWrite).Should(Equal([]string{
+				"organization:{{.organizationID}}$public|boolean:true",
+			}))
+
+			Expect(bundle.GetOperations()[0].AttributesDelete).Should(Equal([]string{
+				"organization:{{.organizationID}}$balance|double:120.900",
+			}))
+		})
+	})
+})
