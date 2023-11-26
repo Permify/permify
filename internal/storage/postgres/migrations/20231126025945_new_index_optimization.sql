178815300f5b060dd86a22c4eba4c7dfbb851547
Saif <shaikhsaifss60@gmail.com>
2023-11-26 03:13:19 -0800

    refactored migration file.

--- internal/storage/postgres/migrations/20231126025945_new_index_optimization.sql
+++ internal/storage/postgres/migrations/20231126025945_new_index_optimization.sql
@@ -1,8 +1,8 @@
 -- +goose NO TRANSACTION
 -- +goose Up
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_attributes_tenant_created_tx ON public.attributes (tenant_id, created_tx_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_attributes_tenant_created_tx ON attributes (tenant_id, created_tx_id);
 DROP INDEX CONCURRENTLY IF EXISTS idx_tuples_entity;
 
 -- +goose Down
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_tuples_entity ON public.relation_tuples (tenant_id, entity_type, entity_id, relation);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_tuples_entity ON relation_tuples (tenant_id, entity_type, entity_id, relation);
 DROP INDEX CONCURRENTLY IF EXISTS idx_attributes_tenant_created_tx;
