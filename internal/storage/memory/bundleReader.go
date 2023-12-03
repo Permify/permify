6778e28319d5ecc85b2d922cc9dd65595e817209
Rutik7066 <rutikthakre@gmail.com>
2023-12-04 02:48:39 +0530

    implemented go-memdb bundle writer and reader

--- internal/storage/memory/bundleReader.go
+++ internal/storage/memory/bundleReader.go
@@ -3,22 +3,58 @@ package memory
 import (
 	"context"
 	"errors"
+	"log/slog"
+	"strings"
 
-	db "github.com/Permify/permify/pkg/database/memory"
+	"github.com/golang/protobuf/jsonpb"
+	"go.opentelemetry.io/otel/codes"
+
+	"github.com/Permify/permify/internal/storage/postgres/utils"
 	base "github.com/Permify/permify/pkg/pb/base/v1"
+	"github.com/hashicorp/go-memdb"
 )
 
-// BundleReader -
 type BundleReader struct {
-	database *db.Memory
+	database *memdb.MemDB
 }
 
-func NewBundleReader(database *db.Memory) *BundleReader {
+func NewBundleReader(database *memdb.MemDB) *BundleReader {
 	return &BundleReader{
 		database: database,
 	}
 }
 
-func (b *BundleReader) Read(_ context.Context, _, _ string) (bundle *base.DataBundle, err error) {
-	return nil, errors.New(base.ErrorCode_ERROR_CODE_NOT_IMPLEMENTED.String())
+func (b *BundleReader) Read(ctx context.Context, tenantID, name string) (bundle *base.DataBundle, err error) {
+	ctx, span := tracer.Start(ctx, "bundle-reader.read-bundle")
+	defer span.End()
+
+	slog.Info("Reading bundle: ", slog.Any("tenant_id", tenantID), slog.Any("name", name))
+
+	txn := b.database.Txn(false)
+	defer txn.Abort()
+
+	raw, err := txn.First("bundle", "id", tenantID, name)
+	if err != nil {
+		return nil, utils.HandleError(span, err, base.ErrorCode_ERROR_CODE_SQL_BUILDER)
+	}
+
+	if raw == nil {
+		return nil, errors.New(base.ErrorCode_ERROR_CODE_BUNDLE_NOT_FOUND.String())
+	}
+
+	jsonData := raw.(*base.DataBundle).String()
+
+	m := jsonpb.Unmarshaler{}
+	bundle = &base.DataBundle{}
+	err = m.Unmarshal(strings.NewReader(jsonData), bundle)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+
+		slog.Error("Failed to convert the value to bundle: ", slog.Any("error", err))
+
+		return nil, errors.New(base.ErrorCode_ERROR_CODE_INVALID_ARGUMENT.String())
+	}
+
+	return bundle, err
 }
--- internal/storage/memory/bundleWriter.go
+++ internal/storage/memory/bundleWriter.go
@@ -2,27 +2,68 @@ package memory
 
 import (
 	"context"
-	"errors"
+	"log/slog"
 
-	db "github.com/Permify/permify/pkg/database/memory"
+	"github.com/Permify/permify/internal/storage/postgres/utils"
 	base "github.com/Permify/permify/pkg/pb/base/v1"
+	"github.com/hashicorp/go-memdb"
 )
 
-// BundleWriter -
 type BundleWriter struct {
-	database *db.Memory
+	database *memdb.MemDB
 }
 
-func NewBundleWriter(database *db.Memory) *BundleWriter {
+func NewBundleWriter(database *memdb.MemDB) *BundleWriter {
 	return &BundleWriter{
 		database: database,
 	}
 }
 
-func (b *BundleWriter) Write(_ context.Context, _ string, _ []*base.DataBundle) (names []string, err error) {
-	return nil, errors.New(base.ErrorCode_ERROR_CODE_NOT_IMPLEMENTED.String())
+func (b *BundleWriter) Write(ctx context.Context, tenantID string, bundles []*base.DataBundle) (names []string, err error) {
+	ctx, span := tracer.Start(ctx, "bundle-writer.write-bundle")
+	defer span.End()
+
+	slog.Info("Writing bundles to the database", slog.Any("number_of_bundles", len(bundles)))
+
+	txn := b.database.Txn(true)
+
+	for _, bundle := range bundles {
+
+		if err := txn.Insert("bundle", bundle); err != nil {
+			txn.Abort()
+			return names, utils.HandleError(span, err, base.ErrorCode_ERROR_CODE_INTERNAL)
+		}
+	}
+
+	txn.Commit()
+
+	slog.Info("Successfully wrote bundles to the database. ", slog.Any("number_of_bundles", len(bundles)))
+
+	return
 }
 
-func (b *BundleWriter) Delete(_ context.Context, _, _ string) (err error) {
-	return errors.New(base.ErrorCode_ERROR_CODE_NOT_IMPLEMENTED.String())
+func (b *BundleWriter) Delete(ctx context.Context, tenantID string) (err error) {
+	ctx, span := tracer.Start(ctx, "bundle-writer.delete-bundle")
+	defer span.End()
+
+	slog.Info("Deleting bundle: ", slog.Any("bundle", tenantID))
+
+	txn := b.database.Txn(true)
+
+	raw, err := txn.First("bundle", "TenantID", tenantID)
+	if err != nil {
+		txn.Abort()
+		return utils.HandleError(span, err, base.ErrorCode_ERROR_CODE_INTERNAL)
+	}
+
+	if err := txn.Delete("bundle", &raw); err != nil {
+		txn.Abort()
+		return utils.HandleError(span, err, base.ErrorCode_ERROR_CODE_INTERNAL)
+	}
+
+	txn.Commit()
+
+	slog.Info("Successfully deleted Bundle")
+
+	return nil
 }
