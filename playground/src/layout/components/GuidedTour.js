f0e0352a5c95303bac0b7d3e06cf46c1f16a2152
Berkeli Halmyradov <berkelih@gmail.com>
2023-12-15 09:28:36 +0000

    playground: refactor guided tour component

--- playground/src/layout/components/GuidedTour.js
+++ playground/src/layout/components/GuidedTour.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { Tour } from "antd";
 
+const Link = ({ href, text }) => (
+  <a href={href} target="_blank" rel="noreferrer noopener">
+    {text}
+  </a>
+);
+
 export default function GuidedTour({ refSchemaEditor, refRelationshipsAndAttributes, refEnforcement }) {
   const [open, setOpen] = useState(!(localStorage.getItem("hasSeenGuidedTour") === "true"));
 
@@ -16,10 +22,7 @@ export default function GuidedTour({ refSchemaEditor, refRelationshipsAndAttribu
       description: (
         <p>
           This environment enables you to create and test your authorization schema within a browser. The Permify playground comprises three main sections: Schema (Authorization Model), Authorization Data, and Enforcement. While we cover these
-          sections in this tour, you can find the complete documentation at &nbsp;
-          <a href="https://docs.permify.co/" target="_blank" rel="noreferrer noopener">
-            docs.permify.co
-          </a>
+          sections in this tour, you can find the complete documentation at <Link href="https://docs.permify.co/" text="docs.permify.co" />.
         </p>
       ),
     },
@@ -28,11 +31,8 @@ export default function GuidedTour({ refSchemaEditor, refRelationshipsAndAttribu
       target: refSchemaEditor.current,
       description: (
         <p>
-          You can create your authorization model in this section with using our domain specific language. We already have a couple of use cases and example that you can choose from the dropdown above. Also, you can check our &nbsp;
-          <a href="https://docs.permify.co/docs/getting-started/modeling/" target="_blank" rel="noreferrer noopener">
-            docs
-          </a>
-          &nbsp; to learn more about how to model authorization in Permify.
+          You can create your authorization model in this section with using our domain specific language. We already have a couple of use cases and example that you can choose from the dropdown above. Also, you can check our{" "}
+          <Link href="https://docs.permify.co/docs/getting-started/modeling/" text="docs" /> to learn more about how to model authorization in Permify.
         </p>
       ),
     },
@@ -42,10 +42,7 @@ export default function GuidedTour({ refSchemaEditor, refRelationshipsAndAttribu
       description: (
         <p>
           You can create sample authorization data to test your authorization logic. For instance, to create a relationship between your defined entities, simply click the 'Add Relationship' button. For further information on data creation, please
-          refer to &nbsp;
-          <a href="https://docs.permify.co/docs/getting-started/sync-data/" target="_blank" rel="noreferrer noopener">
-            docs
-          </a>
+          refer to <Link href="https://docs.permify.co/docs/getting-started/sync-data/" text="docs" />.
         </p>
       ),
     },
@@ -54,11 +51,8 @@ export default function GuidedTour({ refSchemaEditor, refRelationshipsAndAttribu
       target: refEnforcement.current,
       description: (
         <p>
-          Now that we have sample data and a defined schema, let's perform an access check! The YAML in the Enforcement section represents a test scenario for conducting access checks. To learn more about the capabilities of this YAML, refer to:
-          &nbsp;
-          <a href="https://docs.permify.co/docs/playground/#enforcement-access-check-scenarios" target="_blank" rel="noreferrer noopener">
-            access check scenarios
-          </a>
+          Now that we have sample data and a defined schema, let's perform an access check! The YAML in the Enforcement section represents a test scenario for conducting access checks. To learn more about the capabilities of this YAML, refer to:{" "}
+          <Link href="https://docs.permify.co/docs/playground/#enforcement-access-check-scenarios" text="docs" />.
         </p>
       ),
     },
