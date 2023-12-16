6cdd74cda8eacb2c5c88fba84731bbf080e6cd47
Tolga Ozen <mtolgaozen@gmail.com>
2023-12-16 13:34:27 +0300

    fix: remove deprecated 'showArrow' property to resolve errors

--- playground/src/layout/MainLayout.js
+++ playground/src/layout/MainLayout.js
@@ -95,7 +95,7 @@ const MainLayout = ({children, ...rest}) => {
                 </div>
                 <div className="ml-12">
                     <Select className="mr-8" value={label} style={{width: 220}}
-                            onChange={handleSampleChange} showArrow={true}>
+                            onChange={handleSampleChange}>
                         <OptGroup label="Use Cases">
                             <Option key="empty" value="empty">Empty</Option>
                             <Option key="organizations-hierarchies" value="organizations-hierarchies">Organizations & Hierarchies</Option>
