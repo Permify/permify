ba993e6f6bb358bfe20088454cf970c873a40848
mohanish2504 <mohanishp1@gmail.com>
2024-01-02 19:00:31 +0530

    Modification: usging create/drop schema index concurrently

--- internal/storage/postgres/migrations/20231230174418_schema_version_index.sql
+++ internal/storage/postgres/migrations/20231230174418_schema_version_index.sql
@@ -1,9 +1,10 @@
+-- +goose NO TRANSACTION
 -- +goose Up
 -- +goose StatementBegin
-CREATE INDEX IF NOT EXISTS idx_schema_version ON schema_definitions (version);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_schema_version ON schema_definitions (version);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP INDEX IF EXISTS idx_schema_version;
+DROP INDEX CONCURRENTLY IF EXISTS idx_schema_version;
 -- +goose StatementEnd
