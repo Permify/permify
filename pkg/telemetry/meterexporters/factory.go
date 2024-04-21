5c4824dd7524f30559fd8d2a2dce800997df9203
Tyrone Villaluna <avanaur@gmail.com>
2024-04-21 20:33:38 +1000

    More files

--- pkg/telemetry/meterexporters/factory.go
+++ pkg/telemetry/meterexporters/factory.go
@@ -9,8 +9,10 @@ import (
 // ExporterFactory - Create meter exporter according to given params
 func ExporterFactory(name, endpoint string, insecure bool, urlpath string) (metric.Exporter, error) {
 	switch name {
-	case "otlp":
+	case "otlp", "otlp-http":
 		return NewOTLP(endpoint, insecure, urlpath)
+	case "otlp-grpc":
+		return NewOTLPGRPC(endpoint, insecure)
 	default:
 		return nil, fmt.Errorf("%s meter exporter is unsupported", name)
 	}
