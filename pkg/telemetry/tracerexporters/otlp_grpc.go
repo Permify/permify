a4d5b7445469df05e3883a29627a0e3702f7ec12
Tyrone Villaluna <avanaur@gmail.com>
2024-04-21 20:30:59 +1000

    Support OTLP GRPC exporters

---
+++ pkg/telemetry/tracerexporters/otlp_grpc.go
@@ -0,0 +1,32 @@
+package tracerexporters
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/trace"
+	"google.golang.org/grpc/credentials"
+)
+
+// NewOTLPGrpc - Creates new OTLP exporter using GRPC protocol.
+func NewOTLPGrpc(endpoint string, insecure bool) (trace.SpanExporter, error) {
+	var exporter trace.SpanExporter
+	var err error
+
+	opts := []otlptracegrpc.Option{
+		otlptracegrpc.WithEndpoint(endpoint),
+	}
+
+	if insecure {
+		opts = append(opts, otlptracegrpc.WithInsecure())
+	} else {
+		opts = append(opts, otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, "")))
+	}
+
+	exporter, err = otlptracegrpc.New(context.Background(), opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return exporter, nil
+}
