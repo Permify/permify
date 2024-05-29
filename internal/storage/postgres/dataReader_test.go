d9fd2db1591609d58e90e4a5f317c785e60ecb28
Tolga Ozen <mtolgaozen@gmail.com>
2024-05-29 18:34:29 +0300

    Revert "fixed the test"
    This reverts commit 4dc00ecafeea8145185222b9312d8d9eb419f82d.

--- internal/storage/postgres/dataReader_test.go
+++ internal/storage/postgres/dataReader_test.go
@@ -122,7 +122,8 @@ var _ = Describe("DataReader", func() {
 			}, token1.String())
 			Expect(err).ShouldNot(HaveOccurred())
 
-			Expect(it1.HasNext()).Should(Equal(false))
+			Expect(it1.HasNext()).Should(Equal(true))
+			Expect(it1.GetNext()).Should(Equal(tup1))
 			Expect(it1.HasNext()).Should(Equal(false))
 
 			it2, err := dataReader.QueryRelationships(ctx, "t1", &base.TupleFilter{
@@ -133,8 +134,10 @@ var _ = Describe("DataReader", func() {
 			}, token2.String())
 			Expect(err).ShouldNot(HaveOccurred())
 
-			Expect(it2.HasNext()).Should(Equal(false))
-			Expect(it2.HasNext()).Should(Equal(false))
+			Expect(it2.HasNext()).Should(Equal(true))
+			Expect(it2.GetNext()).Should(Equal(tup1))
+			Expect(it2.HasNext()).Should(Equal(true))
+			Expect(it2.GetNext()).Should(Equal(tup3))
 			Expect(it2.HasNext()).Should(Equal(false))
 		})
 	})
@@ -181,7 +184,7 @@ var _ = Describe("DataReader", func() {
 			}, token1.String(), database.NewPagination(database.Size(2), database.Token("")))
 			Expect(err).ShouldNot(HaveOccurred())
 
-			Expect(len(col1.GetTuples())).Should(Equal(0))
+			Expect(len(col1.GetTuples())).Should(Equal(2))
 
 			col2, ct2, err := dataReader.ReadRelationships(ctx, "t1", &base.TupleFilter{
 				Entity: &base.EntityFilter{
@@ -191,7 +194,7 @@ var _ = Describe("DataReader", func() {
 			}, token1.String(), database.NewPagination(database.Size(3), database.Token(ct1.String())))
 			Expect(err).ShouldNot(HaveOccurred())
 
-			Expect(len(col2.GetTuples())).Should(Equal(0))
+			Expect(len(col2.GetTuples())).Should(Equal(3))
 			Expect(ct2.String()).Should(Equal(""))
 
 			token3, err := dataWriter.Delete(ctx, "t1", &base.TupleFilter{
@@ -215,7 +218,7 @@ var _ = Describe("DataReader", func() {
 			}, token3.String(), database.NewPagination(database.Size(4), database.Token("")))
 			Expect(err).ShouldNot(HaveOccurred())
 
-			Expect(len(col3.GetTuples())).Should(Equal(0))
+			Expect(len(col3.GetTuples())).Should(Equal(4))
 			Expect(ct3.String()).Should(Equal(""))
 		})
 	})
@@ -474,19 +477,20 @@ var _ = Describe("DataReader", func() {
 
 			ids1, ct1, err := dataReader.QueryUniqueEntities(ctx, "t1", "organization", token1.String(), database.NewPagination(database.Size(8), database.Token("")))
 			Expect(err).ShouldNot(HaveOccurred())
-			Expect(len(ids1)).Should(Equal(6))
+			Expect(len(ids1)).Should(Equal(8))
 
 			ids2, ct2, err := dataReader.QueryUniqueEntities(ctx, "t1", "organization", token1.String(), database.NewPagination(database.Size(8), database.Token(ct1.String())))
 			Expect(err).ShouldNot(HaveOccurred())
-			Expect(len(ids2)).Should(Equal(6))
+			Expect(len(ids2)).Should(Equal(1))
 			Expect(ct2.String()).Should(Equal(""))
 
 			ids3, ct3, err := dataReader.QueryUniqueEntities(ctx, "t1", "organization", token1.String(), database.NewPagination(database.Size(20), database.Token("")))
 			Expect(err).ShouldNot(HaveOccurred())
-			Expect(len(ids3)).Should(Equal(6))
+			Expect(len(ids3)).Should(Equal(9))
 			Expect(ct3.String()).Should(Equal(""))
-			
-			Expect(isSameArray(ids3, []string{"organization-2", "organization-3", "organization-16", "organization-28", "organization-17", "organization-1"})).Should(BeTrue())
+
+			Expect(isSameArray(ids3, []string{"organization-1", "organization-2", "organization-3", "organization-19", "organization-10", "organization-16", "organization-14", "organization-28", "organization-17"})).Should(BeTrue())
+
 			token2, err := dataWriter.Delete(ctx, "t1",
 				&base.TupleFilter{},
 				&base.AttributeFilter{
@@ -499,10 +503,10 @@ var _ = Describe("DataReader", func() {
 
 			ids4, ct4, err := dataReader.QueryUniqueEntities(ctx, "t1", "organization", token2.String(), database.NewPagination(database.Size(20), database.Token("")))
 			Expect(err).ShouldNot(HaveOccurred())
-			Expect(len(ids4)).Should(Equal(5))
+			Expect(len(ids4)).Should(Equal(8))
 			Expect(ct4.String()).Should(Equal(""))
-			
-			Expect(isSameArray(ids4, []string{ "organization-2", "organization-3", "organization-16","organization-28","organization-1"})).Should(BeTrue())
+
+			Expect(isSameArray(ids4, []string{"organization-1", "organization-2", "organization-3", "organization-19", "organization-10", "organization-16", "organization-14", "organization-28"})).Should(BeTrue())
 		})
 	})
 
@@ -556,14 +560,14 @@ var _ = Describe("DataReader", func() {
 				Relation: "",
 			}, token1.String(), database.NewPagination(database.Size(2), database.Token("")))
 			Expect(err).ShouldNot(HaveOccurred())
-			Expect(len(refs1)).Should(Equal(0))
+			Expect(len(refs1)).Should(Equal(2))
 
 			refs2, ct2, err := dataReader.QueryUniqueSubjectReferences(ctx, "t1", &base.RelationReference{
 				Type:     "user",
 				Relation: "",
 			}, token1.String(), database.NewPagination(database.Size(2), database.Token(ct1.String())))
 			Expect(err).ShouldNot(HaveOccurred())
-			Expect(len(refs2)).Should(Equal(0))
+			Expect(len(refs2)).Should(Equal(2))
 			Expect(ct2.String()).Should(Equal(""))
 
 			refs3, ct3, err := dataReader.QueryUniqueSubjectReferences(ctx, "t1", &base.RelationReference{
@@ -571,16 +575,20 @@ var _ = Describe("DataReader", func() {
 				Relation: "",
 			}, token1.String(), database.NewPagination(database.Size(20), database.Token("")))
 			Expect(err).ShouldNot(HaveOccurred())
-			Expect(len(refs3)).Should(Equal(0))
+			Expect(len(refs3)).Should(Equal(4))
 			Expect(ct3.String()).Should(Equal(""))
+
+			Expect(isSameArray(refs3, []string{"user-1", "user-2", "user-3", "user-5"})).Should(BeTrue())
 
 			refs4, ct4, err := dataReader.QueryUniqueSubjectReferences(ctx, "t1", &base.RelationReference{
 				Type:     "organization",
 				Relation: "member",
 			}, token1.String(), database.NewPagination(database.Size(20), database.Token("")))
 			Expect(err).ShouldNot(HaveOccurred())
-			Expect(len(refs4)).Should(Equal(0))
+			Expect(len(refs4)).Should(Equal(1))
 			Expect(ct4.String()).Should(Equal(""))
+
+			Expect(isSameArray(refs4, []string{"organization-8"})).Should(BeTrue())
 		})
 	})
 })
