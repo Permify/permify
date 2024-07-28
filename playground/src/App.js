73031942555cd6154ff6fe8351f5027d1a9e25de
Tolga Ozen <mtolgaozen@gmail.com>
2024-07-28 09:20:18 +0300

    feat: utility, layout and router aliases added

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import AppRouter from "./router/router";
-import {Layout} from "./utility/context/layout"
+import AppRouter from "@router/router";
+import {Layout} from "@utility/context/layout"
 import {ConfigProvider, theme} from 'antd';
 
 function App() {
