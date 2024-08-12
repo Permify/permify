8189874418e06e6ca15f555a63e24920e899e5b5
ufuk <ufukcivan4@gmail.com>
2024-08-12 17:43:33 +0300

    health endpoint is added to swagger

--- pkg/pb/base/v1/service.pb.validate.go
+++ pkg/pb/base/v1/service.pb.validate.go
@@ -35,6 +35,208 @@ var (
 	_ = sort.Sort
 )
 
+// Validate checks the field values on HealthRequest with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *HealthRequest) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on HealthRequest with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in HealthRequestMultiError, or
+// nil if none found.
+func (m *HealthRequest) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *HealthRequest) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if len(errors) > 0 {
+		return HealthRequestMultiError(errors)
+	}
+
+	return nil
+}
+
+// HealthRequestMultiError is an error wrapping multiple validation errors
+// returned by HealthRequest.ValidateAll() if the designated constraints
+// aren't met.
+type HealthRequestMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m HealthRequestMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m HealthRequestMultiError) AllErrors() []error { return m }
+
+// HealthRequestValidationError is the validation error returned by
+// HealthRequest.Validate if the designated constraints aren't met.
+type HealthRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e HealthRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e HealthRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e HealthRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e HealthRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e HealthRequestValidationError) ErrorName() string { return "HealthRequestValidationError" }
+
+// Error satisfies the builtin error interface
+func (e HealthRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sHealthRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = HealthRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = HealthRequestValidationError{}
+
+// Validate checks the field values on HealthResponse with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *HealthResponse) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on HealthResponse with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in HealthResponseMultiError,
+// or nil if none found.
+func (m *HealthResponse) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *HealthResponse) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Status
+
+	if len(errors) > 0 {
+		return HealthResponseMultiError(errors)
+	}
+
+	return nil
+}
+
+// HealthResponseMultiError is an error wrapping multiple validation errors
+// returned by HealthResponse.ValidateAll() if the designated constraints
+// aren't met.
+type HealthResponseMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m HealthResponseMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m HealthResponseMultiError) AllErrors() []error { return m }
+
+// HealthResponseValidationError is the validation error returned by
+// HealthResponse.Validate if the designated constraints aren't met.
+type HealthResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e HealthResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e HealthResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e HealthResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e HealthResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e HealthResponseValidationError) ErrorName() string { return "HealthResponseValidationError" }
+
+// Error satisfies the builtin error interface
+func (e HealthResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sHealthResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = HealthResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = HealthResponseValidationError{}
+
 // Validate checks the field values on PermissionCheckRequest with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
