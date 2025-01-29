3b6cf0c4375f5156297e77b4af1b4cf8573cb666
Brian Chen <ihasdapi@gmail.com>
2025-01-29 17:48:04 -0500

    fix: checkDepth should throw error if depth <= 0
    Depth can go below 0

--- internal/invoke/utils.go
+++ internal/invoke/utils.go
@@ -9,7 +9,7 @@ import (
 
 // checkDepth - a helper function that returns an error if the depth in a PermissionCheckRequest is zero.
 func checkDepth(request *base.PermissionCheckRequest) error {
-	if atomic.LoadInt32(&request.GetMetadata().Depth) == 0 {
+	if atomic.LoadInt32(&request.GetMetadata().Depth) <= 0 {
 		return errors.New(base.ErrorCode_ERROR_CODE_DEPTH_NOT_ENOUGH.String())
 	}
 	return nil
