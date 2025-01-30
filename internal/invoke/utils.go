68705a0cc9519f8e35d20b7de24e598fe3ba639a
Brian <brian@kortex.co>
2025-01-29 17:40:07 -0800

    Update utils.go

--- internal/invoke/utils.go
+++ internal/invoke/utils.go
@@ -7,7 +7,7 @@ import (
 	base "github.com/Permify/permify/pkg/pb/base/v1"
 )
 
-// checkDepth - a helper function that returns an error if the depth in a PermissionCheckRequest is zero.
+// checkDepth - a helper function that returns an error if the depth in a PermissionCheckRequest is <= zero.
 func checkDepth(request *base.PermissionCheckRequest) error {
 	if atomic.LoadInt32(&request.GetMetadata().Depth) <= 0 {
 		return errors.New(base.ErrorCode_ERROR_CODE_DEPTH_NOT_ENOUGH.String())
